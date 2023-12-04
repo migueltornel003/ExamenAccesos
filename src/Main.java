7c5e019c76e3dcb865ac13ae35a8f04498a0a772
migueltornel003 <migueltornel003@gmail.com>
2023-12-04 09:58:22 +0100

    IMPORTANTE

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,9 @@ import java.util.Scanner;
 
 public class Main {
 	public static void main(String[] args) {
+		//IMPORTANTE
+		//EN EL COMMIT CON DESCRIPCION "Ejercicio 1 parte 1 y menu más intuitivo" es erróneo
+		//Quería decir "Ejercicio 2 parte 1 y menu más intuitivo"
 		Scanner sc = new Scanner(System.in);
 		int option;
 
